e05ebc7c5d751f06516792b4afefb13a6bf1f1b5
Saarim Zaman <saarim.z@gmail.com>
2019-05-07 15:00:50 -0700

    initial commit

---
+++ DjangoLevel5/learning_users/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/saarim/Desktop/workspace/udemy-django/DjangoLevel5/learning_users/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
